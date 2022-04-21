6cfdb67416e7456a31e64f3edebd70d15b78ceb2
WebDev TVlean <tvleanjohnny@gmail.com>
2022-04-21 12:59:58 +0300

    new lesson

--- Продвинутый JS/Lesson1/project/js/main.js
+++ Продвинутый JS/Lesson1/project/js/main.js
@@ -3,10 +3,11 @@ const products = [
     {id: 2, title: 'Mouse', price: 100},
     {id: 3, title: 'Keyboard', price: 250},
     {id: 4, title: 'Gamepad', price: 150},
+    {id: 5},
 ];
 
-const getProductHTMLString = (title, price) => {
-    return `<div class="product-item">
+const getProductHTMLString = (title = 'Нет заголовка', price = 'Цена не указана') => {
+    return `<div class="product-item" >
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="by-btn">Добавить</button>
@@ -14,10 +15,26 @@ const getProductHTMLString = (title, price) => {
 };
 
 const renderProducts = (productList) => {
-  const list = productList.map((good) => getProductHTMLString(good.title, good.price));
-
-  document.querySelector('.products').innerHTML = list;
-    // console.log(list);
+  for(var i = 0; i < productList.map((good) => getProductHTMLString(good.title, good.price)).length; i++) {
+    document.querySelector('.products').innerHTML += productList.map((good) => getProductHTMLString(good.title, good.price))[i];
+  }
 }
+
+// const renderProducts = (productList) => {
+//   const list = productList.map((good) => getProductHTMLString(good.title, good.price));
+//   for(var i = 0; i < list.length; i++) {
+//     document.querySelector('.products').innerHTML += list[i];
+//   }
+// }
+
+
+  /*
+    ОТВЕТ 1:
+    Сделал два вида решения, оставил тот вариант, который смог сократить в строковом количестве.
+    ОТВЕТ 2:
+    Запятая выводиться из за метода map как я понимаю, он собирает новый массив и при этом оставляет запятую. 
+    я подумал что логичнее через цикл вывести все элементы, но не уверен что это правильно, почему то мне кажется,
+    что есть более простой способ =) Я пробежался по другим методам массивов, но не нашел ни чего подходящего.
+  */
 
 renderProducts(products);
